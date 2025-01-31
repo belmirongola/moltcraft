1282310a18e4695a76edd1b356f557ebc0a46714
Vitaly Turovsky <vital2580@icloud.com>
2025-01-31 05:02:22 +0300

    up code

--- src/assembly.ts
+++ src/assembly.ts
@@ -1,10 +1,10 @@
 import { _createChunkHandle, _getLightAt, _updateLightAt } from './MCLight'
 
-window.getBlockAt = (...args) => {
-  console.log('getBlockAt', args)
+window.getBlockAt = (x, y, z) => {
+  console.log('getBlockAt', x, y, z)
 }
-window.getChunkHandleAt = (...args) => {
-  console.log('getChunkHandleAt', args)
+window.getChunkHandleAt = window.getChunkAt = (x, z) => {
+  console.log('getChunkHandleAt', x, z)
 }
 
 window.wasm = {
@@ -18,12 +18,19 @@ window.getChunkAt = (x, z) => {
 }
 
 window.testWasm = () => {
-  const handle = 0
+  const handle = window.wasm._createChunkHandle(0, 0, 0, 0, 256)
+  if (handle <= 0) {
+    console.error('Failed to create chunk handle')
+    return
+  }
+
   const X = 0
   const Y = 0
   const Z = 0
   const ambientDarkness = 0
-  window.wasm._createChunkHandle(handle, 0, 0, -64, 256)
-  window.wasm._updateLightAt(handle, X, Y, Z)
-  window.wasm._getLightAt(handle, X, Y, Z, ambientDarkness)
+  // window.wasm._updateLightAt(handle, X, Y, Z)
+  // window.wasm._getLightAt(handle, X, Y, Z, ambientDarkness)
+  // getLightAt(chunk_handle_ptr, x_in, y_in, z_in, ambient_darkness_in)
+  const lightLevel = window.wasm._getLightAt(handle, X, Y, Z, ambientDarkness)
+  console.log('lightLevel', lightLevel)
 }
