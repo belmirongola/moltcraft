183e7d8c938070f3950396df9a877f49d99ada8b
Vitaly Turovsky <vital2580@icloud.com>
2025-03-26 01:10:43 +0300

    add modal

---
+++ src/react/AboutModal.tsx
@@ -0,0 +1,15 @@
+export default () => {
+  return (
+    <div>
+      <h2>Minecraft Open Source Edition</h2>
+      <small><i>What if Minecraft was an online game?</i></small>
+      <p>
+        Hi! You are on the safest and fast modern Minecraft clone rewritten in JS. A huge work was done in the project, however many features would not be possible without these awesome people:
+      </p>
+      <p>
+        - Everyone who provided awesome mods for the game
+        - Gen for rewriting the physics engine to be Grim-compliant
+      </p>
+    </div>
+  )
+}
