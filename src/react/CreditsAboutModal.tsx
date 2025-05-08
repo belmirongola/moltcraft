97f1d18a44e91420c53cc25384b110f373ab8181
Vitaly Turovsky <vital2580@icloud.com>
2025-05-08 21:03:43 +0300

    add links

---
+++ src/react/CreditsAboutModal.tsx
@@ -0,0 +1,22 @@
+export default () => {
+  return (
+    <div>
+      <h2>Minecraft Open Source Edition</h2>
+      <small><i>What if Minecraft was an online game?</i></small>
+      <p>
+        Hey! You are on the safest and fast modern Minecraft clone rewritten in JS. A huge work was done in the project, however many features would not be possible without these awesome projects:
+      </p>
+      <p>
+        - Everyone who provided awesome mods for the game
+        - [Gen](https://discord.com/users/gen6442) for rewriting the physics engine to be Grim-compliant
+        - [ViaVersion](https://viaversion.com/) for providing reliable sound id mappings
+        - [Bluemap](https://github.com/BlueMap-Minecraft/BlueMap) for providing block entity models like chest
+        - [Deepslate](https://github.com/misode/deepslate) for rendering 3d blocks in GUI (inventory)
+        - [skinview3d](https://www.npmjs.com/package/skinview3d) for rendering skins & player geometry
+        - [Polymer](https://github.com/atxi/Polymer) (c++ project) for providing fast & accurate server light implementation
+
+        Major contributors:
+      </p>
+    </div>
+  )
+}
