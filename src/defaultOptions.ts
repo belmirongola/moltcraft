7a692ac210e53eedb3a6de19b61ed55fcf14264e
Vitaly Turovsky <vital2580@icloud.com>
2025-06-29 15:22:58 +0300

    f

--- src/defaultOptions.ts
+++ src/defaultOptions.ts
@@ -42,6 +42,11 @@ export const defaultOptions = {
   starfieldRendering: true,
   enabledResourcepack: null as string | null,
   useVersionsTextures: 'latest',
+  // Instanced rendering options
+  useInstancedRendering: false,
+  forceInstancedOnly: false,
+  instancedOnlyDistance: 6,
+  enableSingleColorMode: false,
   serverResourcePacks: 'prompt' as 'prompt' | 'always' | 'never',
   showHand: true,
   viewBobbing: true,
